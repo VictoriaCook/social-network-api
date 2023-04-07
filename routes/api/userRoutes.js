b970838e7e4226552305042c95e8ad6a61e1cd26
VC <117161646+VictoriaCook@users.noreply.github.com>
2023-04-07 14:05:00 +1000

    set up user routes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+
+const {
+    allUsers, 
+    createUser,
+    userById, 
+    deleteUser,
+    updateUser,
+    addFriend,
+    deleteFriend
+} = require('../../controllers/userController');
+
+// .../api/users
+router.route('/').get(allUsers).post(createUser);
+
+// .../api/users/:id
+router.route('/:id').get(userById).delete(deleteUser).put(updateUser);
+
+// .../api/users/:id/friends/:friendId
+router.route('/:id/friends/:friendId').post(addFriend).delete(deleteFriend);
+
+module.exports = router; 
